6627a895370090b3de2b4bcae45b80be40ad4d78
Mohamad-sakib <158846349+Mohamad-sakib@users.noreply.github.com>
2025-02-02 02:03:09 +0530

    FoodDeliverySystem is done

--- foodDeliverySystem/src/main2.js
+++ foodDeliverySystem/src/main2.js
@@ -12,19 +12,35 @@ const timeDiff = (initalTime, currentTime) => {
   return ((currentTime - initalTime) / 1000).toFixed(2);
 };
 
-// const deliverOrder = task(
-//   (time) => display(`[${timeDiff(time, Date.now())}s] Delvering order`),
-//   (time) => display(`[${timeDiff(time, Date.now())}s] Order delivered`),
-//   5000,
-//   () => {}
-// );
+const deliverOrder = task(
+  (time) => display(`[${timeDiff(time, Date.now())}s] Delvering order`),
+  (time, orderDetails) => {
+    const updatedDetails = {
+      ...orderDetails,
+      ...{ deliveryDetails: "Delivered by John at 7:30 PM" },
+    };
+    display(`[${timeDiff(time, Date.now())}s] Order delivered`, updatedDetails);
 
-// const packOrder = task(
-//   (time) => display(`[${timeDiff(time, Date.now())}s] Packing Order`),
-//   (time) => display(`[${timeDiff(time, Date.now())}s] Order packed`),
-//   3000,
-//   deliverOrder
-// );
+    return orderDetails;
+  },
+  5000,
+  () => {}
+);
+
+const packOrder = task(
+  (time) => display(`[${timeDiff(time, Date.now())}s] Packing Order`),
+  (time, orderDetails) => {
+    const updatedDetails = {
+      ...orderDetails,
+      ...{ packageDetails: "Packed in eco-friendly box" },
+    };
+
+    display(`[${timeDiff(time, Date.now())}s] Order packed`, updatedDetails);
+    return updatedDetails;
+  },
+  3000,
+  deliverOrder
+);
 
 const prepareFood = task(
   (time) => display(`[${timeDiff(time, Date.now())}s] Preparing food`),
@@ -38,8 +54,7 @@ const prepareFood = task(
   },
 
   2000,
-  () => {}
-  // packOrder
+  packOrder
 );
 
 const receiveOrder = task(
@@ -52,55 +67,11 @@ const receiveOrder = task(
   prepareFood
 );
 
-receiveOrder(Date.now(), { orderId: Math.floor(Math.random() * 1000) });
-// const deliveringOrder = task(
-//   () => {},
-//   () => display("Delivering order"),
-//   1000,
-//   deliverOrder
-// );
+const main = () => {
+  const orderId = Math.floor(Math.random() * 1000);
+  const orderTime = Date.now();
 
-// const packOrder = task(
-//   () => {},
-//   () => display("Order is packed"),
-//   3000,
-//   deliveringOrder
-// );
+  receiveOrder(orderTime, { orderId });
+};
 
-// const packingOrder = task(
-//   () => {},
-//   () => display("Packing order"),
-//   1000,
-//   packOrder
-// );
-
-// const foodPrepared = task(
-//   () => {},
-//   () => display("Food is prepared"),
-//   3000,
-//   packingOrder
-// );
-
-// const preparingFood = task(
-//   () => {},
-//   () => display("Preparing food"),
-//   1000,
-//   foodPrepared
-// );
-
-// const orderReceived = task(
-//   () => display("Order received"),
-//   () => {},
-//   1000,
-//   preparingFood
-// );
-
-// orderReceived();
-
-// [0.00s] Order received: { orderId: 123 }// Date.now() start
-// [3.01s] Preparing food...//
-// [3.01s] Food is ready: { orderId: 123, foodDetails: 'Burger & Fries' }
-// [5.02s] Packing order...
-// [5.02s] Order packed: { orderId: 123, foodDetails: 'Burger & Fries', packageDetails: 'Packed in eco-friendly box' }
-// [10.04s] Delivering order...
-// [10.04s] Order delivered: { orderId: 123, foodDetails: 'Burger & Fries', packageDetails: 'Packed in eco-friendly box', deliveryDetails: 'Delivered by John at 7:30 PM' }
+main();
