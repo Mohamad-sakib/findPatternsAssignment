d186fa400e207558a9e3a8b85319c857baafbde2
Mohamad-sakib <158846349+Mohamad-sakib@users.noreply.github.com>
2025-03-23 08:12:52 +0530

    connection betwenn server and client established

--- serverClietn2/client.js
+++ serverClietn2/client.js
@@ -1,1 +1,4 @@
-const conn = Deno.connect({ port: 8000 });
+const conn = await Deno.connect({ port: 8000 });
+await conn.write(new TextEncoder().encode("ADD 1 1"));
+const response = await conn.readable.getReader().read();
+Deno.stdout.write(response.value);
